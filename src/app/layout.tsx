95143caa50ce11e6b2d92a68a65f75b1c458006d
devemmy001 <devemmy001@gmail.com>
2025-12-21 09:05:03 +0100

    added halenoir font universally

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,23 @@
 import type { Metadata } from "next";
-import { Montserrat } from "next/font/google";
+import localFont from "next/font/local";
 import "./globals.css";
 
-const montserrat = Montserrat({
-  subsets: ["latin"],
-});
-
-const montserratMono = Montserrat({
-  variable: "--font-montserrat-mono",
-  subsets: ["latin"],
+const halenoir = localFont({
+  src: [
+    { path: "../../public/font/Halenoir-Regular.otf", weight: "400", style: "normal" },
+    { path: "../../public/font/Halenoir-RegularItalic.otf", weight: "400", style: "italic" },
+    { path: "../../public/font/Halenoir-Medium.otf", weight: "500", style: "normal" },
+    { path: "../../public/font/Halenoir-SemiBold.otf", weight: "600", style: "normal" },
+    { path: "../../public/font/Halenoir-Bold.otf", weight: "700", style: "normal" },
+    { path: "../../public/font/Halenoir-ExtraBold.otf", weight: "800", style: "normal" },
+    { path: "../../public/font/Halenoir-Black.otf", weight: "900", style: "normal" },
+    { path: "../../public/font/Halenoir-Thin.otf", weight: "100", style: "normal" },
+    { path: "../../public/font/Halenoir-UltraLight.otf", weight: "200", style: "normal" },
+    { path: "../../public/font/Halenoir-ExtraLight.otf", weight: "300", style: "normal" },
+    { path: "../../public/font/Halenoir-BlackItalic.otf", weight: "900", style: "italic" },
+  ],
+  variable: "--font-halenoir",
+  display: "swap",
 });
 
 export const metadata: Metadata = {
@@ -23,10 +32,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      
-      <body
-        className={`${montserrat.className} ${montserratMono.variable} antialiased tracking-tighter`}
-      >
+      <body className={`${halenoir.className} ${halenoir.variable} antialiased tracking-tighter`}>
         {children}
       </body>
     </html>
